a2edfcaa6f806b692b72bfaaf3f5798430606f6d
Kyle Bartsch <kyleb8@gmail.com>
2026-01-30 12:00:01 -0700

    namespace refactoring, add stat annotation helper functions

--- app/logic/shared/plot_utils.R
+++ app/logic/shared/plot_utils.R
@@ -1,17 +1,23 @@
-#' Helper function to clone volcano plot traces for pathway / GSEA analysis
-#' Calls custom JS scripts which can be found in `inst/assets/js/script.js`
-#' @param session - shiny session object - session for target plot
-#' @param ns - NS - namespace - not currently used
-#' @param plot_name - string - name of target plot - defaults to "VolcanoPlot"
-#' @param r6 - R6 class - R6 class with namespace logic
-#' @param action - string - if "add", will clone and add traces to volcano plot, otherwise will remove traces
-#' @importFrom shinyjs runjs
-#' @importFrom glue glue 
+box::use(
+  glue[glue, glue_collapse],
+  shinyjs[runjs],
+  tibble[tibble],
+  tidyr[separate],
+  dplyr[select, filter, distinct, pull, mutate],
+  stringr[str_split_i],
+  rlang[enquo],
+  purrr[imap, map2, compact]
+)
+box::use(
+  app/logic/shared/statistical_analysis[formatPValue]
+)
+
+#' @export
 toggle_GSEA_volcano_plot_trace <- function(
-    session, 
-    ns, 
+    session,
+    ns,
     namespace,
-    plot_name = "VolcanoPlot", 
+    plot_name = "VolcanoPlot",
     expected_trace_count = 3,
     analytes = "",
     trace_name = "GSEA Pathway",
@@ -25,30 +31,23 @@ toggle_GSEA_volcano_plot_trace <- function(
     object_name = plot_name
   )
 
-  shinyjs::runjs(glue::glue('App.resetPlotTraceVisibility("{namespaced_plot_name}");'))
+  runjs(glue('App.resetPlotTraceVisibility("{namespaced_plot_name}");'))
 
-  shinyjs::runjs(glue::glue('App.removeExcessPlotTraces("{namespaced_plot_name}",{expected_trace_count});'))
+  runjs(glue('App.removeExcessPlotTraces("{namespaced_plot_name}",{expected_trace_count});'))
 
-  shinyjs::runjs(glue::glue('App.removeExcessAnnotations("{namespaced_plot_name}",5);'))
-  shinyjs::runjs(glue::glue('App.clearSelectedKeys("{namespaced_plot_name}");'))
+  runjs(glue('App.removeExcessAnnotations("{namespaced_plot_name}",5);'))
+  runjs(glue('App.clearSelectedKeys("{namespaced_plot_name}");'))
 
   if (action == "add") {
 
-    shinyjs::runjs(glue::glue('App.cloneTraceByKeys("{namespaced_plot_name}","{analytes}","{trace_name}");'))
-    shinyjs::runjs(glue::glue('App.isolateTraceVisibility("{namespaced_plot_name}","{trace_name}");'))
+    runjs(glue('App.cloneTraceByKeys("{namespaced_plot_name}","{analytes}","{trace_name}");'))
+    runjs(glue('App.isolateTraceVisibility("{namespaced_plot_name}","{trace_name}");'))
 
   }
 
 }
 
-#' Helper function to purge plotly plot
-#' Calls custom JS scripts which can be found in `inst/assets/js/script.js`
-#' @param session - shiny session object
-#' @param ns - NS - namespace - not currently used
-#' @param plot_name - string - name of target plot
-#' @param r6 - R6 class - R6 class with namespace logic
-#' @importFrom shinyjs runjs 
-#' @importFrom glue glue
+#' @export
 purge_plot <- function(session, ns, plot_name, r6) {
 
   plotName <- get_object_name_from_namespace_session(
@@ -57,65 +56,59 @@ purge_plot <- function(session, ns, plot_name, r6) {
     object_name = plot_name
   )
 
-  shinyjs::runjs(glue::glue('App.PurgePlot("{plotName}");'))
+  runjs(glue('App.PurgePlot("{plotName}");'))
 
 }
 
+#' @export
 object_is_rendered <- function(session, target_obj_name) {
   return(
-    tibble::tibble(
+    tibble(
       "obj_name"  = names(session$clientData)
     ) |>
-    tidyr::separate(
+    separate(
       obj_name,
       into = c("type", "object", "property", "value"),
       sep = "_", extra = "drop", fill = "right"
     ) |>
-    dplyr::filter(tolower(object) == tolower(target_obj_name)) |>
-    dplyr::distinct(object) |>
-    dplyr::pull() |>
+    filter(tolower(object) == tolower(target_obj_name)) |>
+    distinct(object) |>
+    pull() |>
     length() |>
     as.logical()
   )
 }
 
-#' Helper function to find and return fully-qualified / namespaced object name
-#' @param session - shiny session object
-#' @param namespace - string - namespace for target object
-#' @param object_name - string - name of target object
-#' @import dplyr
-#' @import tibble
-#' @import tidyr
-#' @importFrom stringr str_split_i
+#' @export
 get_object_name_from_namespace_session <- function(session, namespace, object_name) {
 
   if (is.null(session) || is.null(namespace) || is.null(object_name)) {
     stop("session, namespace, and object_name are required parameters")
   }
 
-  matching_objects <- tibble::tibble(
+  matching_objects <- tibble(
     "obj_name"  = names(session$clientData)
     ) |>
-    tidyr::separate(
+    separate(
         obj_name,
-        into = c("type", "object", "property"), 
+        into = c("type", "object", "property"),
         sep = "_", extra = "drop", fill = "right"
     ) |>
-    tidyr::separate(
+    separate(
         object, into = c("app", "Parent", "id", "module", "output"), remove = FALSE,
         sep = "-", extra = "drop", fill = "right"
     ) |>
-    dplyr::filter(
+    filter(
         tolower(id) == tolower(namespace),
         property != "hidden"
     ) |>
-    dplyr::filter(output == object_name) |>
-    dplyr::distinct(object)
+    filter(output == object_name) |>
+    distinct(object)
 
   if (nrow(matching_objects) == 1) {
     return(
       matching_objects |>
-        dplyr::pull()
+        pull()
     )
   }
   if (nrow(matching_objects) > 1) {
@@ -125,7 +118,166 @@ get_object_name_from_namespace_session <- function(session, namespace, object_na
   }
 }
 
-
+#' @export
 get_namespace_by_level <- function(namespace, level = 1) {
-  return(stringr::str_split_i(namespace, "-", level))
+  return(str_split_i(namespace, "-", level))
+}
+
+#' @export
+getStatAnnotationAnchorLines <- function(
+  .data,
+  group,
+  group_members,
+  significance_variable,
+  group_is_significant,
+  include_insignificant_values = FALSE
+) {
+
+  group <- enquo(group)
+  significance_variable <- enquo(significance_variable)
+  group_is_significant <- enquo(group_is_significant)
+
+  data_group_memebers <- .data |>
+    select(!!group) |>
+    distinct() |>
+    pull()
+
+  all_group_members <- intersect(group_members, data_group_memebers)
+
+  line_template <- list(
+    type = "line",
+    x = 0,
+    y = 0,
+    line = list(
+      color = "grey"
+    ),
+    xref = "x",
+    yref = "paper",
+    showarrow = FALSE
+  )
+
+  return(
+    map2(
+      all_group_members,
+      seq(-0.2, length(all_group_members) - 1, 1),
+      function(x, xpos) {
+        stat_result <- .data |>
+          filter(!!group == x) |>
+          distinct(!!significance_variable) |>
+          pull()
+
+        is_significant <- .data |>
+          filter(!!group == x) |>
+          select(!!group_is_significant) |>
+          distinct() |>
+          pull()
+
+        if (!is.na(stat_result) || include_insignificant_values) {
+          line <- line_template
+          line[["x0"]] <- xpos
+          line[["x1"]] <- xpos + 0.5
+          line[["y0"]] <- 1
+          line[["y1"]] <- 1
+          line[["statResult"]] <- stat_result
+          line[["isSignificant"]] <- is_significant
+          line[["group"]] <- x
+          line
+        } else {
+          NULL
+        }
+      }
+    ) |>
+      compact()
+  )
+}
+
+#' @export
+getGroupedStatAnnotations <- function(
+  AnnotationAnchorLines,
+  statTest,
+  covariates,
+  adjustmentMethod,
+  ...
+  ) {
+
+  annotation_template <- list(
+    type = "line",
+    x = 0,
+    y = 0,
+    line = list(
+      color = "blue"
+    ),
+    font = list(
+      family = "Arial",
+      color = "rgb(58, 62, 65)",
+      size = 12
+    ),
+    text = "",
+    xref = "x",
+    yref = "paper",
+    showarrow = FALSE
+  )
+
+  annotations <- imap(
+      AnnotationAnchorLines,
+      function(x, i) {
+        annotation <- annotation_template
+        stat_result <- AnnotationAnchorLines[[i]]$statResult
+        annotation[["x"]] <- AnnotationAnchorLines[[i]]$x1 -
+          (AnnotationAnchorLines[[i]]$x1 - (AnnotationAnchorLines[[i]]$x0) / 2)
+        annotation[["y"]] <- 1.05
+        annotation[["text"]] <- dplyr::case_when(
+          is.na(stat_result) ~ "NA",
+          !AnnotationAnchorLines[[i]]$isSignificant ~ "ns",
+          stat_result <= 0.001 ~  "***",
+          stat_result <= 0.01 ~ "**",
+          TRUE ~ "*"
+        )
+        annotation[["hovertext"]] <- formatPValue(stat_result, adjustmentMethod, ...)
+        return(annotation)
+      }
+    ) |>
+      compact()
+
+  keyText <- ifelse(
+    adjustmentMethod != "none",
+    "<span><b>Statistical Significance Key</b>:        ns q > 0.1         
+     * q <= 0.1         ** q <= 0.01        *** q <= 0.001</span>",
+    "<span><b>Statistical Significance Key</b>:        ns p > 0.05        
+      * p <= 0.05        ** p <= 0.01        *** p <= 0.001</span>"
+  )
+
+  adjLetterString <- ifelse(adjustmentMethod == "none", "p", "q")
+  covariatesString <- ifelse(
+    !is.null(covariates) & statTest == "Linear Model",
+    glue(' adjusted for {glue_collapse(covariates,", ", last = " and ")}'),
+    ""
+  )
+  correctionString <- ifelse(
+    adjustmentMethod == "none",
+    "",
+    glue(", corrected using the {adjustmentMethod} method")
+  )
+
+  hoverText <- glue("{adjLetterString}-values calculated based on a {statTest}{covariatesString}{correctionString}.")
+
+  significanceKey <- list(
+    type = "line",
+    align = "left",
+    x = 0.5,
+    y = 1.12,
+    hovertext = hoverText,
+    text = keyText,
+    font = list(
+      size = 12
+    ),
+    xref = "paper",
+    yref = "paper",
+    showarrow = FALSE
+  )
+
+  annotations <- c(annotations, list(significanceKey))
+
+  return(annotations)
+
 }
