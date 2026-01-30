3af0043f435c9ff4422c083ba78489949a8b846e
Kyle Bartsch <kyleb8@gmail.com>
2026-01-29 17:43:05 -0700

    linting

--- app/view/custom_ui/feedback_tools.R
+++ app/view/custom_ui/feedback_tools.R
@@ -4,55 +4,52 @@ box::use(
     dplyr[all_of]
 )
 
-
 #' @export
-createTooltip <- function(Text, URL, TooltipText, ShowTooltip=TRUE, ...) {
+createTooltip <- function(Text, URL, TooltipText, ShowTooltip = TRUE, ...) {
 
     if (!ShowTooltip) {
         return(
-            shiny::HTML(
-                glue::glue('<div>{Text}</div>')
+            HTML(
+                glue("<div>{Text}</div>")
             )
         )
-    }
-    else {
+    } else {
 
-        if (URL != "" & !is.na(URL)) {
-        return(
-            shiny::HTML(
-                glue::glue(
-                    '<div>{Text}
-                        <span
-                            data-html="true"
-                            onclick="window.open(\'{URL}\');"
-                            data-toggle="tooltip"
-                            data-placement="auto right"
-                            title=""
-                            class="fas fa-info-circle gtooltip info-tooltip"
-                            data-original-title="{TooltipText}">
-                        </span>
-                    </div>'
+        if (URL != "" && !is.na(URL)) {
+            return(
+                HTML(
+                    glue(
+                        "<div>{Text}
+                            <span
+                                data-html=\"true\"
+                                onclick=\"window.open(\'{URL}\');\"
+                                data-toggle=\"tooltip\"
+                                data-placement=\"auto right\"
+                                title=\"\"
+                                class=\"fas fa-info-circle gtooltip info-tooltip\"
+                                data-original-title=\"{TooltipText}\">
+                            </span>
+                        </div>"
+                    )
                 )
             )
-        )
-        }
-        else {
-        return(
-            shiny::HTML(
-                glue::glue(
-                    '<div>{Text}
-                        <span
-                            data-html="true"
-                            data-toggle="tooltip"
-                            data-placement="auto right"
-                            title=""
-                            class="fas fa-info-circle gtooltip info-tooltip"
-                            data-original-title="{TooltipText}">
-                        </span>
-                    </div>'
+        } else {
+            return(
+                HTML(
+                    glue(
+                        "<div>{Text}
+                            <span
+                                data-html=\"true\"
+                                data-toggle=\"tooltip\"
+                                data-placement=\"auto right\"
+                                title=\"\"
+                                class=\"fas fa-info-circle gtooltip info-tooltip\"
+                                data-original-title=\"{TooltipText}\">
+                            </span>
+                        </div>"
+                    )
                 )
             )
-        )
         }
     }
 }
