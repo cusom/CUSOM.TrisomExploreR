98cfad484ff7072e6f0f82b0a229c00d83ec54fd
Kyle Bartsch <kyleb8@gmail.com>
2026-01-30 17:18:01 -0700

    analyte data manager factory checks if anaysis is precalcualted and assigns appropriate subclass

--- app/logic/analyte_plots/AnalyteDataManagerFactory.R
+++ app/logic/analyte_plots/AnalyteDataManagerFactory.R
@@ -1,10 +1,12 @@
 box::use(
     app/logic/analyte_plots/CategoricalAnalyteDataManager[CategoricalFeatureAnalysisAnalyteDataManager],
+    app/logic/analyte_plots/PreCalculatedCategoricalAnalyteDataManager[PreCalculatedCategoricalFeatureAnalysisAnalyteDataManager],
     app/logic/analyte_plots/ContinuousAnalyteDataManager[ContinuousFeatureAnalysisAnalyteDataManager]
 )
 
 #' @export
 getFeatureAnalysisAnalyteDataManager <- function(
+    app_config, 
     analysis_config,
     study,
     study_data,
@@ -12,19 +14,31 @@ getFeatureAnalysisAnalyteDataManager <- function(
     summary_data
     ) {
     if (analysis_config$AnalysisType == "Categorical") {
-        return(
-            CategoricalFeatureAnalysisAnalyteDataManager$new(
+        if (analysis_config$UsesPreCalculatedData) {
+            PreCalculatedCategoricalFeatureAnalysisAnalyteDataManager$new(
+                app_config = app_config,
                 analysis_config = analysis_config,
                 study = study,
                 study_data = study_data,
                 analyte = analyte,
                 summary_data = summary_data
             )
-        )
-
+        } else {
+            return(
+                CategoricalFeatureAnalysisAnalyteDataManager$new(
+                    app_config = app_config,
+                    analysis_config = analysis_config,
+                    study = study,
+                    study_data = study_data,
+                    analyte = analyte,
+                    summary_data = summary_data
+                )
+            )
+        }
     } else if (analysis_config$AnalysisType == "Continuous") {
         return(
             ContinuousFeatureAnalysisAnalyteDataManager$new(
+                app_config = app_config,
                 analysis_config = analysis_config,
                 study = study,
                 study_data = study_data,
