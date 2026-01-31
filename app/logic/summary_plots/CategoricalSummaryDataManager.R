b23d867d43f15ebe51f43f31448af3dba452286c
Kyle Bartsch <kyleb8@gmail.com>
2026-01-30 17:15:41 -0700

    summary data manager factory checks if analysis is precalcualted and assigns appropriate class

--- app/logic/summary_plots/CategoricalSummaryDataManager.R
+++ app/logic/summary_plots/CategoricalSummaryDataManager.R
@@ -3,26 +3,19 @@ box::use(
   app/logic/shared/statistical_analysis[getStatTestByKeyGroup, formatPValue, addGroupCount],
 )
 
-
 #' @export
 FeatureAnalysis_CategoricalSummaryDataManager <- R6::R6Class(
   "FeatureAnalysis_CategoricalSummaryDataManager",
   inherit = FeatureAnalysis_SummaryDataManager,
   private = list(),
   public = list(
-
     initialize = function(analysis_config, StatTest, Covariates, AdjustmentMethod) {
-
       super$initialize(analysis_config, StatTest, Covariates, AdjustmentMethod)
-  
     },
-  
     setVolcanoSummaryData = function(.data) {
-    
       if (is.null(.data)) {
         .data <- self$StudyData
       }
-
       self$VolcanoSummaryData <- .data |>
         dplyr::select(LabID, Analyte, log2MeasuredValue, self$analysisVariable, self$Covariates()) |>
         dplyr::mutate_at(dplyr::vars(self$analysisVariable), ~forcats::fct_relevel(.x, self$groupBaselineLabel)) |>
@@ -35,11 +28,8 @@ FeatureAnalysis_CategoricalSummaryDataManager <- R6::R6Class(
           testMethod = self$StatTest(),
           adjustmentMethod = self$AdjustmentMethod(),
           covariates = self$Covariates()
-        ) 
-
+        )
       return(self$VolcanoSummaryData)
-
     }
-
   )
 )
--- app/logic/summary_plots/SummaryDataManagerFactory.R
+++ app/logic/summary_plots/SummaryDataManagerFactory.R
@@ -1,17 +1,26 @@
 box::use(
     app/logic/summary_plots/SummaryDataManager[FeatureAnalysis_SummaryDataManager],
     app/logic/summary_plots/CategoricalSummaryDataManager[FeatureAnalysis_CategoricalSummaryDataManager],
+    app/logic/summary_plots/PreCalculatedCategoricalSummaryDataManager[PreCalculatedFeatureAnalysis_CategoricalSummaryDataManager],
     app/logic/summary_plots/ContinuousSummaryDataManager[FeatureAnalysis_ContinuousSummaryDataManager]
 )
 
 #' @export
 getFeatureAnalysisSummaryDataManager <- function(analysis_config, StatTest, Covariates, AdjustmentMethod) {
     if (analysis_config$AnalysisType == "Categorical") {
-        return(
-            FeatureAnalysis_CategoricalSummaryDataManager$new(
-                analysis_config, StatTest, Covariates, AdjustmentMethod
+        if (analysis_config$UsesPreCalculatedData) {
+            return(
+                PreCalculatedFeatureAnalysis_CategoricalSummaryDataManager$new(
+                    analysis_config, StatTest, Covariates, AdjustmentMethod
+                )
             )
-        )
+        } else {
+            return(
+                FeatureAnalysis_CategoricalSummaryDataManager$new(
+                    analysis_config, StatTest, Covariates, AdjustmentMethod
+                )
+            )
+        }
     } else if (analysis_config$AnalysisType == "Continuous") {
         return(
             FeatureAnalysis_ContinuousSummaryDataManager$new(
