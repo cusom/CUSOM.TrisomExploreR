bebefb318735621d36475927475d1c1b945d006e
Kyle Bartsch <kyleb8@gmail.com>
2026-02-02 18:03:00 -0700

    updates to analyte input module to align with parent runner class

--- app/view/inputs/inputs_volcano_plot_analyte.R
+++ app/view/inputs/inputs_volcano_plot_analyte.R
@@ -12,7 +12,7 @@ ui <- function(id) {
     shiny::tags$span(
       id = ns("AnalyteInput"),
       shiny::selectizeInput(
-        inputId = ns("Analyte"),
+        inputId = ns("analyte"),
         label = "",
         choices = NULL,
         multiple = TRUE,
@@ -61,18 +61,11 @@ ui <- function(id) {
 #' @import glue
 #' @importFrom shinyjs runjs
 #' @export
-server <- function(id, r6, VolcanoSummaryData, plot_click_data, plot_selected_data, parent) {
+server <- function(id, r6, summary_data, plot_click_data, plot_selected_data, summary_plot_name, parent) {
 
   shiny::moduleServer(id, function(input, output, session) {
 
     ns <- session$ns
-
-    server_utils$bind_events(
-      ids = c("Analyte"),
-      r6 = r6,
-      session = session,
-      parent_input = input
-    )
 
     shiny::insertUI(
       session = parent,
@@ -82,48 +75,39 @@ server <- function(id, r6, VolcanoSummaryData, plot_click_data, plot_selected_da
       ui = shiny::htmlOutput(ns("volcanoMultiSelectText"))
     )
 
-    shiny::observeEvent(c(VolcanoSummaryData()), {
-
+    shiny::observeEvent(c(summary_data()), {
       shiny::validate(
-        shiny::need(!is.null(VolcanoSummaryData()), "")
+        shiny::need(!is.null(summary_data()), "")
       )
-
       shiny::isolate({
-
-        analytes <- VolcanoSummaryData() |>
+        analytes <- summary_data() |>
           dplyr::select(Analyte) |>
           dplyr::distinct() |>
           dplyr::arrange(Analyte) |>
           dplyr::pull()
 
-        if (!any(r6$Analyte %in% analytes)) {
-          r6$Analyte <- ""
-          gargoyle::trigger("show_analyte_plot", session = session)
-        }
-
         shiny::updateSelectizeInput(
           session = session,
-          inputId = "Analyte",
+          inputId = "analyte",
           choices = analytes,
-          selected = r6$Analyte,
+          selected = NULL,
           options = list(
             maxOptions = length(analytes)
           ),
           server = TRUE
         )
-
       })
 
     }, domain = session)
 
     shiny::observeEvent(
       c(plot_click_data()), {
-    
-      r6$volcanoEventData <- plot_click_data()
+      plot_click_data() |>
+        r6$set_plot_event_data()
 
       shiny::updateSelectizeInput(
         session = session,
-        inputId = "Analyte",
+        inputId = "analyte",
         selected = plot_click_data()$key
       )
 
@@ -131,70 +115,62 @@ server <- function(id, r6, VolcanoSummaryData, plot_click_data, plot_selected_da
 
     shiny::observeEvent(
       c(plot_selected_data()), {
-  
-      r6$volcanoEventData <- plot_selected_data()
+        plot_selected_data() |>
+          r6$set_plot_event_data()
 
-      shiny::updateSelectizeInput(
-        session = session,
-        inputId = "Analyte",
-        selected = plot_selected_data()$key
-      )
+        shiny::updateSelectizeInput(
+          session = session,
+          inputId = "analyte",
+          selected = plot_selected_data()$key
+        )
 
     }, domain = session)
 
     volcano_multi_select_text <- shiny::reactive({
-
-      r6$volcanoMultiSelectText
-
+        r6$volcanoMultiSelectText
     }) |>
-      shiny::bindEvent(input$Analyte, ignoreInit = TRUE)
+      shiny::bindEvent(input$analyte, ignoreInit = TRUE)
 
     output$volcanoMultiSelectText <- shiny::renderText({
       shiny::HTML(volcano_multi_select_text())
     })
 
-    shiny::observeEvent(c(input$Analyte), {
-
-      r6$annotate_volcano_point(
-        "VolcanoPlot",
-        parent$ns
-      )
-
+    shiny::observeEvent(c(input$analyte), {
+      r6$set_analyte(input$analyte, annotate = TRUE, plot_name = summary_plot_name)
     }, ignoreInit = TRUE, domain = session)
 
-    analyte_search_error_text <- shiny::eventReactive(
-      c(input$analyteSearchResults, input$Analyte), {
-      search_result_data <- input$analyteSearchResults
-      shiny::req(search_result_data)
+    # analyte_search_error_text <- shiny::eventReactive(
+    #   c(input$analyteSearchResults, input$analyte), {
+    #   search_result_data <- input$analyteSearchResults
+    #   shiny::req(search_result_data)
 
-      if (length(input$Analyte) > 0) {
-        shiny::HTML("")
-      } else if (search_result_data$total == 0) {
-        shiny::HTML(
-          paste0(
-            '<span style="color:black;font-size:smaller;padding-left:10px;"><b>"',
-            search_result_data$query,
-            '"</b> not found. Please try another value</span>'
-          )
-        )
-      } else {
-        shiny::HTML("")
-      }
-    }, domain = session)
+    #   if (length(input$analyte) > 0) {
+    #     shiny::HTML("")
+    #   } else if (search_result_data$total == 0) {
+    #     shiny::HTML(
+    #       paste0(
+    #         '<span style="color:black;font-size:smaller;padding-left:10px;"><b>"',
+    #         search_result_data$query,
+    #         '"</b> not found. Please try another value</span>'
+    #       )
+    #     )
+    #   } else {
+    #     shiny::HTML("")
+    #   }
+    # }, domain = session)
 
-    output$AnalyteSearchError <- shiny::renderUI({
-      analyte_search_error_text()
-    })
+    # output$AnalyteSearchError <- shiny::renderUI({
+    #   analyte_search_error_text()
+    # })
 
-
-    Analyte <- shiny::eventReactive(c(input$Analyte), {
-      input$Analyte
-    })
+    # Analyte <- shiny::eventReactive(c(input$Analyte), {
+    #   input$Analyte
+    # })
 
     return(
       list(
-        analyte = Analyte,
-        analyte_input_name ="Analyte",
+        analyte = shiny::reactive({input$analyte}),
+        analyte_input_name = "analyte",
         analyte_session = session
       )
     )
