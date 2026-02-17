6a07c01a92e36d96f17928e85cbee5222aa32a2d
Kyle Bartsch <kyleb8@gmail.com>
2026-02-17 10:12:05 -0700

    add comorbidity data preparer

--- app/logic/feature_analysis/inputs/InputsDataPreparers.R
+++ app/logic/feature_analysis/inputs/InputsDataPreparers.R
@@ -3,7 +3,10 @@ box::use(
     glue[glue, glue_collapse],
     tibble[tibble],
     dplyr[select, filter, between, mutate, group_by, summarise, ungroup, rename_with,
-            distinct, n, pull, arrange, dense_rank, row_number, if_else],
+            distinct, n, pull, arrange, dense_rank, row_number, if_else, inner_join,
+            case_when],
+    tidyr[drop_na],
+    forcats[fct_relevel],
     purrr[pmap],
     stringr[str_split],
     rlang[sym]
@@ -12,7 +15,8 @@ box::use(
 InputsDataPreparerBase <- R6Class(
     "InputsDataPreparerBase",
     private = list(
-        analysis_config = NULL
+        analysis_config = NULL,
+        app_config = NULL
     ),
     active = list(
         analysisVariable = function(value) {
@@ -27,8 +31,9 @@ InputsDataPreparerBase <- R6Class(
     ),
     public = list(
         data = NULL,
-        initialize = function(analysis_config) {
+        initialize = function(analysis_config, app_config = NULL) {
             private$analysis_config <- analysis_config
+            private$app_config <- app_config
         },
         prepare = function() {
             stop("implement prepare")
@@ -43,8 +48,8 @@ FeatureAnalysisInputsDataPreparer <- R6Class(
     private = list(),
     active = list(),
     public = list(
-        initialize = function(analysis_config) {
-            super$initialize(analysis_config)
+        initialize = function(analysis_config, app_config) {
+            super$initialize(analysis_config, app_config)
         },
         prepare = function(.data, study, karyotypes, sexes, ages) {
             k_vec <- str_split(karyotypes, pattern = ";", simplify = FALSE) |>
@@ -67,6 +72,86 @@ FeatureAnalysisInputsDataPreparer <- R6Class(
 
             return(invisible(self$data))
 
+        }
+    )
+)
+
+#' @export
+FeatureAnalysisInputsComorbidityDataPreparer <- R6Class(
+    "FeatureAnalysisInputsComorbidityDataPreparer",
+    inherit = InputsDataPreparerBase,
+    private = list(
+        app_config = NULL
+    ),
+    active = list(
+        remote_files = function(value) {
+            return(private$app_config$remote_files)
+        },
+        participant_conditions = function(value) {
+            return(
+                self$remote_files$get_remote_file_data("conditions")
+            )
+        }
+    ),
+    public = list(
+        initialize = function(analysis_config, app_config) {
+            super$initialize(analysis_config, app_config)
+            private$app_config <- app_config
+        },
+        prepare = function(.data, study, karyotypes, sexes, ages, conditions) {
+
+            k_vec <- str_split(karyotypes, pattern = ";", simplify = FALSE) |>
+                unlist() |>
+                trimws() |>
+                unique()
+
+            # precompute conditions vector (if `conditions` is a tibble)
+            conds <- conditions |> pull() |> unique()
+
+            # Build once: per-record_id flag with SAME semantics you had:
+            # - If ANY NA found among that record_id's rows → NA (and will be dropped below)
+            # - Else "Yes" if ANY True, otherwise "No"
+            cond_flags <- self$participant_conditions |>
+                filter(Condition %in% conds) |>
+                mutate(
+                    has_flag = case_when(
+                        HasCondition == "True"  ~ TRUE,
+                        HasCondition == "False" ~ FALSE,
+                        TRUE                    ~ NA
+                    )
+                ) |>
+                summarise(
+                    HasAnyConditionFlag = {
+                        if (any(is.na(has_flag))) {
+                            NA_character_
+                        } else if (any(has_flag)) {
+                            "Yes"
+                        } else {
+                            "No"
+                        }
+                    },
+                    .by = record_id
+                ) |>
+                tidyr::drop_na(HasAnyConditionFlag)
+
+            # Main pipeline
+            self$data <- .data |>
+                select(record_id, LabID, Karyotype, Sex, Age, BMI, Analyte, MeasuredValue, Measurement) |>
+                filter(
+                    between(Age, ages[1], ages[2]),
+                    Sex %in% sexes,
+                    Karyotype %in% k_vec
+                ) |>
+                mutate(
+                    # preserve original behavior: 0 → 0, positive → log2(value)
+                    log2MeasuredValue = if_else(MeasuredValue == 0, 0.0, log2(MeasuredValue)),
+                    log2Measurement   = sprintf("log<sub>2</sub>(%s)", Measurement)
+                ) |>
+                inner_join(cond_flags, by = "record_id") |>
+                mutate(
+                    HasAnyConditionFlag = factor(HasAnyConditionFlag, levels = c("No", "Yes"))
+                )
+            return(invisible(self$data))
         }
     )
 )
