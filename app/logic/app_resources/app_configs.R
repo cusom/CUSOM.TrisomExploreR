4e66d95d503f2ea191190bf0fd01e93749e454db
Kyle Bartsch <kyleb8@gmail.com>
2026-03-16 15:22:38 -0600

    add app config router and tofa subclass for app config

--- app/logic/app_resources/app_configs.R
+++ app/logic/app_resources/app_configs.R
@@ -8,13 +8,79 @@ box::use(
 )
 
 #' @export
+create_app_settings <- function(application_id,
+                                app_config = NULL,
+                                config_file_name = "config.yml") {
+
+  class_name <- "TrisomExplorerAppManager"
+  class_module <- "app/logic/app_resources/app_configs"
+
+  if (!is.null(app_config$app_settings_class) && nzchar(app_config$app_settings_class)) {
+    class_name <- app_config$app_settings_class
+  }
+
+  if (!is.null(app_config$app_settings_class_module) && nzchar(app_config$app_settings_class_module)) {
+    class_module <- app_config$app_settings_class_module
+  }
+
+  class_generator <- NULL
+
+  if (identical(class_name, "TrisomExplorerAppManager") &&
+      identical(class_module, "app/logic/app_resources/app_configs")) {
+    class_generator <- TrisomExplorerAppManager
+  } else {
+    eval(parse(text = glue::glue("box::use({class_module}[{class_name}])")), envir = environment())
+    class_generator <- get(class_name, envir = environment(), inherits = TRUE)
+  }
+
+  init_args <- app_config$app_settings_args
+
+  if (is.null(init_args)) {
+    init_args <- list()
+  }
+
+  if (!is.list(init_args)) {
+    stop("app_settings_args must be a list.", call. = FALSE)
+  }
+
+  if (is.null(init_args$application_id)) {
+    init_args$application_id <- application_id
+  }
+
+  if (is.null(init_args$config_file_name)) {
+    init_args$config_file_name <- config_file_name
+  }
+
+  do.call(class_generator$new, init_args)
+}
+
+#' @export
 TrisomExplorerAppManager <- R6::R6Class(
   "TrisomExplorerAppManager",
   private = list(
 
   ),
   active = list(
-
+    inputs = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("inputs.json")
+      )
+    },
+    participant_data = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("participants")
+      )
+    },
+    encounter_data = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("encounter")
+      )
+    },
+    condition_data = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("conditions")
+      )
+    }
   ),
   public = list(
     application_id = NULL,
@@ -69,8 +135,14 @@ TrisomExplorerAppManager <- R6::R6Class(
     #' @param ApplicationId - string - application id
     #' @param remoteDB R6 class - query manager for remote database queries
     #' @param localDB R6 class - query manager for local database queries
-    initialize = function(application_id, config_file_name = "config.yml") {
-
+    initialize = function(
+      application_id,
+      config_file_name = "config.yml",
+      load_inputs = TRUE,
+      load_participant_data = TRUE,
+      load_encounter_data = TRUE,
+      load_condition_data = TRUE
+    ) {
       self$application_id <- application_id
 
       self$remote_db <- ODBCQueryManager$new(
@@ -126,40 +198,57 @@ TrisomExplorerAppManager <- R6::R6Class(
           AnalysisVariableBaselineLabel, AnalysisVolcanoPlotTopAnnotation
         )
 
-      inputs <- self$remote_files$get_remote_file_data("inputs.json")
+      if (load_inputs) {
+        self$load_inputs()
+      }
 
-      self$input_config$studies <- inputs$study_choices |>
+      if (load_participant_data) {
+        self$load_participant_data()
+      }
+
+      if (load_encounter_data) {
+        self$load_encounter_data()
+      }
+
+      if (load_condition_data) {
+        self$load_condition_data()
+      }
+
+    },
+
+    load_inputs = function() {
+      self$input_config$studies <- self$inputs$study_choices |>
           as.data.frame()
 
       self$input_config$statTestschoiceNames <- purrr::pmap(
-        inputs$stat_tests,
+        self$inputs$stat_tests,
         ui_utils$createTooltip
       )
 
       self$input_config$adjustmentMethodsNames <- purrr::pmap(
-        inputs$adj_methods,
+        self$inputs$adj_methods,
         ui_utils$createTooltip
       )
 
-      self$input_config$platforms <- inputs$platforms
+      self$input_config$platforms <- self$inputs$platforms
 
-      self$input_config$experimentIDs <- inputs$experiment_ids
+      self$input_config$experimentIDs <- self$inputs$experiment_ids
+    },
 
-      participant_data <- self$remote_files$get_remote_file_data("participants")
-
-      self$input_config$karyotypes <- participant_data |>
+    load_participant_data = function() {
+      self$input_config$karyotypes <- self$participant_data |>
         dplyr::collect() |>
         dplyr::distinct(Karyotype) |>
         dplyr::pull()
 
-      self$input_config$sexes <- participant_data |>
+      self$input_config$sexes <- self$participant_data |>
         dplyr::collect() |>
         dplyr::distinct(Sex) |>
         dplyr::pull()
+    },
 
-      encounter_data <- self$remote_files$get_remote_file_data("encounter")
-
-      self$input_config$ages <- encounter_data |>
+    load_encounter_data = function() {
+      self$input_config$ages <- self$encounter_data |>
         dplyr::collect() |>
         tidyr::drop_na() |>
         dplyr::summarise(
@@ -170,15 +259,15 @@ TrisomExplorerAppManager <- R6::R6Class(
           age = seq(min, max, 1)
         ) |>
         dplyr::pull()
+    },
 
-      condition_data <- self$remote_files$get_remote_file_data("conditions")
-
-      self$input_config$Conditions <- condition_data |>
+    load_condition_data = function() {
+      self$input_config$Conditions <- self$condition_data |>
         dplyr::collect() |>
         dplyr::distinct(Condition) |>
         dplyr::pull()
 
-      self$input_config$ConditionClasses <- condition_data |>
+      self$input_config$ConditionClasses <- self$condition_data |>
         dplyr::collect() |>
         dplyr::distinct(ConditionClass) |>
         tidyr::separate_rows(sep = ";", "ConditionClass", convert = TRUE) |>
@@ -187,7 +276,7 @@ TrisomExplorerAppManager <- R6::R6Class(
         dplyr::distinct() |>
         dplyr::pull()
 
-      self$input_config$ConditionChoices <- condition_data |>
+      self$input_config$ConditionChoices <- self$condition_data |>
         dplyr::collect() |>
         dplyr::filter(HasCondition == "True") |>
         dplyr::select(LabID, ConditionClass, Condition) |>
@@ -196,7 +285,7 @@ TrisomExplorerAppManager <- R6::R6Class(
         dplyr::summarize(n = dplyr::n_distinct(LabID), .groups = "drop")  |>
         dplyr::filter(n >= 5) |>
         dplyr::left_join(
-          condition_data |>
+          self$condition_data |>
             dplyr::collect() |>
             dplyr::filter(!is.na(ConditionCensorshipAgeGroup)) |>
             dplyr::distinct(Condition, ConditionCensorshipAgeGroup)
@@ -211,7 +300,6 @@ TrisomExplorerAppManager <- R6::R6Class(
         ) |>
         dplyr::select(-ConditionCensorshipAgeGroup, n) |>
         tidyr::separate_rows(sep = ";", "ConditionClass", convert = TRUE)
-
     },
 
     get_module_config = function(namespace) {
@@ -229,6 +317,62 @@ TrisomExplorerAppManager <- R6::R6Class(
 
     get_input_config = function(namespace) {
       return(self$input_config)
+    }
+  )
+)
+
+
+#' @export
+TOFAAppManager <- R6::R6Class(
+  "TOFAAppManager",
+  inherit = TrisomExplorerAppManager,
+  private = list(
+
+  ),
+  active = list(
+    inputs = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("inputs.json")
+      )
+    },
+    participant_data = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("PARTICIPANTS")
+      )
+    },
+    encounter_data = function(value) {
+      return(
+        self$remote_files$get_remote_file_data("VISITS")
+      )
+    }
+  ),
+  public = list(
+    initialize = function(application_id, config_file_name = "config.yml") {
+      super$initialize(application_id, config_file_name, TRUE, TRUE, TRUE, FALSE)
+
+      # additional initialization for TOFA app can go here
+
+    },
+    load_participant_data = function() {
+      self$input_config$karyotypes <- self$participant_data |>
+        dplyr::distinct(DownSyndromeStatus) |>
+        dplyr::pull()
+
+      self$input_config$sexes <- self$participant_data |>
+        dplyr::distinct(Sex) |>
+        dplyr::pull()
+    },
+    load_encounter_data = function() {
+      self$input_config$ages <- self$encounter_data |>
+        tidyr::drop_na() |>
+        dplyr::summarise(
+          min = round(min(Age_at_visit_in_days)),
+          max = round(max(Age_at_visit_in_days)) + 1
+        ) |>
+        dplyr::reframe(
+          age = seq(min, max, 1)
+        ) |>
+        dplyr::pull()
     }
   )
 )
