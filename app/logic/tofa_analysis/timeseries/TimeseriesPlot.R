6f0a24526c26d6028b39d19241b1fc0242dc5139
Kyle Bartsch <kyleb8@gmail.com>
2026-04-22 08:48:57 -0600

    add dataset to plot interface, pass dataset and analysis_config args. Add base and subclasses for plot data manager classes

--- app/logic/tofa_analysis/timeseries/TimeseriesPlot.R
+++ app/logic/tofa_analysis/timeseries/TimeseriesPlot.R
@@ -1,11 +1,11 @@
 box::use(
     R6[R6Class],
-    dplyr[if_else]
+    dplyr[if_else, case_when]
 )
 
 box::use(
     app/logic/shared/factory_routing[resolve_class],
-    app/logic/tofa_analysis/timeseries/TimeseriesPlotDataManagers[BaseTimeseriesDataSource],
+    app/logic/tofa_analysis/timeseries/TimeseriesPlotDataManagers[EndpointsTimeSeriesDataSource, NULISATimeSeriesDataSource, OLINKTimeSeriesDataSource],
     app/logic/tofa_analysis/timeseries/TimeseriesPlotDataPreparers[BaseTimeseriesPreparer, DifferenceTimeseriesPreparer],
     app/logic/tofa_analysis/timeseries/TimeseriesPlotStrategies[BasePlotStrategy, DifferencePlotStrategy]
 )
@@ -14,33 +14,35 @@ getPlotKind <- function(analysis_type) {
     return("BoxPlot")
 }
 
-getDataSource <- function(type, dataset, cohort, feature, ...) {
+getDataSource <- function(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...) {
 
     map <- list(
-        Base            = BaseTimeseriesDataSource,
-        Difference      = BaseTimeseriesDataSource
+        endpoints           = EndpointsTimeSeriesDataSource,
+        nulisa              = NULISATimeSeriesDataSource,
+        olink               = OLINKTimeSeriesDataSource
     )
-    cls <- resolve_class(map, type, "DataSource")
-    cls$new(type, dataset, cohort, feature, ...)
+    cls <- resolve_class(map, dataset_type, "DataSource")
+    cls$new(analysis_config, plot_type, dataset, cohort, feature, ...)
 }
 
-getPreparer <- function(type, dataset, cohort, feature, ...) {   
+getPreparer <- function(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...) {
+
     map <- list(
         Base            = BaseTimeseriesPreparer,
         Difference      = DifferenceTimeseriesPreparer
     )
-    cls <- resolve_class(map, type, "Preparer")
-    cls$new(type, dataset, cohort, feature, ...)
+    cls <- resolve_class(map, plot_type, "Preparer")
+    cls$new(plot_type, dataset, cohort, feature, ...)
 }
 
-getPlotStrategy <- function(type, dataset, cohort, feature, ...) {
+getPlotStrategy <- function(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...) {
 
     map <- list(
         Base            = BasePlotStrategy,
         Difference      = DifferencePlotStrategy
     )
-    cls <- resolve_class(map, type, "PlotStrategy")
-    cls$new(type, dataset, cohort, feature, ...)
+    cls <- resolve_class(map, plot_type, "PlotStrategy")
+    cls$new(plot_type, dataset, cohort, feature, ...)
 }
 
 
@@ -61,28 +63,36 @@ TimeseriesPlotRunner <- R6Class(
             self$plotter <- plotter
         },
         get_plot = function(dataset, cohort, feature, plot_type) {
-    
+
             data <- self$data_source$get_data(dataset, cohort, feature)
             prepared_data <- self$preparer$prepare(data)
             plot <- self$plotter$render(prepared_data)
             return(plot)
-            
+
         }
     )
 )
 
 #' @export
 getTimeseriesPlot <- function(
-        type, 
+        analysis_config,
+        plot_type,
         dataset,
         cohort,
         feature,
         ...
     ) {
 
-    data_src   <- getDataSource(type, dataset, cohort, feature, ...)
-    preparer   <- getPreparer(type, dataset, cohort, feature, ...)
-    plotter    <- getPlotStrategy(type, dataset, cohort, feature, ...)
+    dataset_type <- case_when(
+        grepl("Endpoints", dataset, ignore.case = TRUE) ~ "endpoints",
+        grepl("Nulisa", dataset, ignore.case = TRUE) ~ "nulisa",
+        grepl("Olink", dataset, ignore.case = TRUE) ~ "olink",
+        TRUE ~ "other"
+    )
+
+    data_src   <- getDataSource(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...)
+    preparer   <- getPreparer(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...)
+    plotter    <- getPlotStrategy(analysis_config, dataset_type, plot_type, dataset, cohort, feature, ...)
 
     TimeseriesPlotRunner$new(
         data_src,
--- app/logic/tofa_analysis/timeseries/TimeseriesPlotDataManagers.R
+++ app/logic/tofa_analysis/timeseries/TimeseriesPlotDataManagers.R
@@ -3,29 +3,87 @@ box::use(
     dplyr[inner_join, join_by, filter],
 )
 
-#' @export
+
 BaseTimeseriesDataSource <- R6Class(
     "BaseTimeseriesDataSource",
-    active = list(),
+    private = list(
+        analysis_config = NULL
+    ),
+    active = list(
+        remote_files = function(value) {
+            return(private$analysis_config$remote_files)
+        }
+    ),
     public = list(
         type = NULL,
         dataset = NULL,
         cohort = NULL,
         feature = NULL,
         data = NULL,
-        initialize = function(type, dataset, cohort, feature, ...) {
+        initialize = function(analysis_config, type, dataset, cohort, feature, ...) {
+            private$analysis_config <- analysis_config
             self$type <- type
             self$dataset <- dataset
             self$cohort <- cohort
             self$feature <- feature
         },
         get_data = function(...) {
+            stop("get_data not implemented for BaseTimeseriesDataSource")
+        }
+    )
+)
+
+#' @export
+EndpointsTimeSeriesDataSource <- R6Class(
+    "EndpointsTimeSeriesDataSource",
+    inherit = BaseTimeseriesDataSource,
+    active = list(
+        time_series_data = function(value) {
+            return(
+                self$remote_files$get_experiment_data(self$dataset)
+            )
+        }
+    ),
+    public = list(
+        initialize = function(analysis_config, type, dataset, cohort, feature, ...) {
+            super$initialize(analysis_config, type, dataset, cohort, feature, ...)
+        },
+        get_data = function(...) {
+            # For endpoints, we want to join the cohort to the dataset first, then filter by feature
             self$data <- self$cohort |>
                 inner_join(
-                    self$dataset |>
-                        filter(Feature == self$feature) ,
+                    self$time_series_data,
                     join_by(Internal_ParticipantID, TOFA_LabID)
-                ) 
+                ) |>
+                filter(Feature == self$feature)
+            return(invisible(self$data))
+        }
+    )
+)
+
+#' @export
+NULISATimeSeriesDataSource <- R6Class(
+    "NULISATimeSeriesDataSource",
+    inherit = BaseTimeseriesDataSource,
+    public = list(
+        initialize = function(analysis_config, type, dataset, cohort, feature, ...) {
+            super$initialize(analysis_config, type, dataset, cohort, feature, ...)
+        },
+        get_data = function(...) {
+            return(invisible(self$data))
+        }
+    )
+)
+
+#' @export
+OLINKTimeSeriesDataSource <- R6Class(
+    "OLINKTimeSeriesDataSource",
+    inherit = BaseTimeseriesDataSource,
+    public = list(
+        initialize = function(analysis_config, type, dataset, cohort, feature, ...) {
+            super$initialize(analysis_config, type, dataset, cohort, feature, ...)
+        },
+        get_data = function(...) {
             return(invisible(self$data))
         }
     )
