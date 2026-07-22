563e0f2271427effbe3678041f033b2bee3f885d
kbartsch <kyleb83@gmail.com>
2026-07-22 09:17:58 -0600

    remove all R package artifacts

--- R/TrisomExploreR-package.R
+++
@@ -1,5 +0,0 @@
-.onLoad <- function(...) {
-  utils::data("GSEA_hallmarks", "states_shp", package = "TrisomExploreR", envir = parent.env(environment()))
-  shiny::addResourcePath('custom-assets', system.file('assets', package = 'TrisomExploreR'))
-  shiny::addResourcePath('www', system.file('www', package = 'TrisomExploreR'))
-}
